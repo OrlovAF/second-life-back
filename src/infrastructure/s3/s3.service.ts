faed7299d270f8529725f09930e74eac9e36cfcd
OrlovAF <orlov84.af@gmail.com>
2026-04-14 13:09:25 +0300

    feat: s3 service presigned put

--- src/infrastructure/s3/s3.service.ts
+++ src/infrastructure/s3/s3.service.ts
@@ -1,12 +1,43 @@
-import { S3Client } from '@aws-sdk/client-s3';
-import { Inject, Injectable } from '@nestjs/common';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { S3_CLIENT } from './s3.provider';
 
 @Injectable()
 export class S3Service {
-  constructor(
-    @Inject(S3_CLIENT) private readonly s3: S3Client,
-    private readonly config: ConfigService,
-  ) {}
+  private client: S3Client;
+
+  constructor(private readonly config: ConfigService) {
+    this.client = new S3Client({
+      region: config.get<string>('s3.region'),
+      endpoint: config.get<string>('s3.endpoint'),
+      forcePathStyle: true,
+      credentials: {
+        accessKeyId: config.get<string>('s3.accessKey'),
+        secretAccessKey: config.get<string>('s3.secretKey'),
+      },
+    });
+  }
+
+  async createPresignedPutUrl(params: {
+    key: string;
+    contentType: string;
+    checksum: string;
+    expiresIn?: number;
+  }) {
+    const bucket = this.config.get<string>('s3.bucket');
+
+    const command = new PutObjectCommand({
+      Bucket: bucket,
+      Key: params.key,
+      ContentType: params.contentType,
+      ChecksumSHA256: params.checksum,
+    });
+
+    const url = await getSignedUrl(this.client, command, {
+      expiresIn: params.expiresIn ?? 60,
+    });
+
+    return url;
+  }
 }
